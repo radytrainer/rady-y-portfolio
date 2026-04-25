8f8612984a6dd102d0cfa3ec980e12acce0793ce
radytrainer <rady.y@passerellesnumeriques.org>
2026-04-25 13:02:20 +0700

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((reg) => {
+    navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`).then((reg) => {
       console.log('SW registered:', reg);
     }).catch((err) => {
       console.log('SW registration failed:', err);
